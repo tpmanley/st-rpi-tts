6fea0673836c7c71e6efe3336204142bf0ba7463
Tom Manley <tom.manley@gmail.com>
2016-03-26 05:36:32 +0000

    Added API to get speaker volume

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
+import subprocess
 from flask import Flask, render_template, Response
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/tts/<int:door>/open')
-def open_door(door):
-    print "Open %s" % door
-    doors[door].open()
-    return "OK"
+class Volume(Resource):
+    def get(self):
+        return subprocess.check_output("amixer get PCM|grep -o [0-9]*%|sed 's/%//'", shell=True).strip()
+
+#    def put(self, vol):
+#        subprocess.call("sudo amixer set PCM -- %s" % vol, shell=True)
+
+api.add_resource(Volume, '/speaker/volume')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0') #, debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
